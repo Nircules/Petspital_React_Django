b6c57086de491b0f80b0695ec5732e99eee68c70
Nircules <nirbengiat@gmail.com>
2023-03-14 18:05:18 +0200

    Fixed minor error

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -10,13 +10,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  <ProSidebarProvider>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
-  </ProSidebarProvider>
-  // {/* </React.StrictMode> */}
+  <React.StrictMode>
+    <ProSidebarProvider>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </ProSidebarProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
